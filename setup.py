050f8765f0652787b05a0dc901bd68baee34ecd9
Mexson Fernandes <robomex2020@gmail.com>
2020-12-14 23:12:29 +0530

    :frog: ready with basic template

--- setup.py
+++ setup.py
@@ -5,34 +5,35 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
-
 version = '2020.0.0'
 
 install_requires = [
-    # List your project dependencies here.
-    # For more details, see:
-    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+    "asgiref==3.2.5",
+    "autocorrect==0.4.4",
+    "Django>=2.0.7",
+    "opencv-python==4.2.0.32",
+    "pytz==2019.3",
+    "sqlparse==0.3.1"
 ]
 
-
 setup(name='djanyolo',
-    version=version,
-    description="Manage content for YOLO model.",
-    long_description=README + '\n\n' + NEWS,
-    classifiers=[
-      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    ],
-    keywords='yolo, djanyolo, django',
-    author='Mexson Fernandes',
-    author_email='robomex2020@gmail.com',
-    url='mexsonfernandes.com',
-    license='MIT',
-    packages=find_packages('src'),
-    package_dir = {'': 'src'},include_package_data=True,
-    zip_safe=False,
-    install_requires=install_requires,
-    entry_points={
-        'console_scripts':
-            ['djanyolo=djanyolo:main']
-    }
+      version=version,
+      description="Manage content for YOLO model.",
+      long_description=README + '\n\n' + NEWS,
+      classifiers=[
+          # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      ],
+      keywords='yolo, djanyolo, django',
+      author='RoboMx Team',
+      author_email='ask@robomx.tech',
+      url='https://robomx.com',
+      license='MIT',
+      packages=find_packages('src'),
+      package_dir={'': 'src'}, include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      entry_points={
+          'console_scripts':
+              ['djanyolo=djanyolo:main']
+      }
 )
